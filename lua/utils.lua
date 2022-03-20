3a4c608b176ec3d503e702e470502df01b1d7a25
Martin Kozlovsky <kozlovsky.m7@gmail.com>
2022-03-21 00:00:01 +0100

    changed structure

--- lua/utils.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-
-M.Color = require("colorbuddy.color").Color
-M.Group = require("colorbuddy.group").Group
-M.colors = require("colorbuddy.color").colors
-M.styles = require("colorbuddy.style").styles
-
-return M
