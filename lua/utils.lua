35946234c75b432874e32cda6fd479acc18fe892
Martin Kozlovsky <kozlovsky.m7@gmail.com>
2022-03-20 23:52:09 +0100

    first commit

---
+++ lua/utils.lua
@@ -0,0 +1,8 @@
+local M = {}
+
+M.Color = require("colorbuddy.color").Color
+M.Group = require("colorbuddy.group").Group
+M.colors = require("colorbuddy.color").colors
+M.styles = require("colorbuddy.style").styles
+
+return M
