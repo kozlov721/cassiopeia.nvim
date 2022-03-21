d286ffaf49b8e110b7f2df24325c7f352e882d80
Martin Kozlovsky <kozlovsky.m7@gmail.com>
2022-03-21 01:28:24 +0100

    changed lualine return

--- lua/lualine/cassiopeia.lua
+++ lua/lualine/cassiopeia.lua
@@ -11,35 +11,37 @@ local colors = {
   inactivegray = '#1C1E26',
 }
 
-return {
-  normal = {
-    a = { bg = colors.gray, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.lightgray, fg = colors.white },
-    c = { bg = colors.darkgray, fg = colors.white },
-  },
-  insert = {
-    a = { bg = colors.blue, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.lightgray, fg = colors.white },
-    c = { bg = colors.darkgray, fg = colors.white },
-  },
-  visual = {
-    a = { bg = colors.yellow, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.lightgray, fg = colors.white },
-    c = { bg = colors.darkgray, fg = colors.white },
-  },
-  replace = {
-    a = { bg = colors.red, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.lightgray, fg = colors.white },
-    c = { bg = colors.darkgray, fg = colors.white },
-  },
-  command = {
-    a = { bg = colors.green, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.lightgray, fg = colors.white },
-    c = { bg = colors.darkgray, fg = colors.white },
-  },
-  inactive = {
-    a = { bg = colors.inactivegray, fg = colors.gray, gui = 'bold' },
-    b = { bg = colors.inactivegray, fg = colors.gray },
-    c = { bg = colors.inactivegray, fg = colors.gray },
-  },
+local cassiopeia = {}
+
+cassiopeia.normal = {
+  a = { bg = colors.gray, fg = colors.black, gui = 'bold' },
+  b = { bg = colors.lightgray, fg = colors.white },
+  c = { bg = colors.darkgray, fg = colors.white },
 }
+cassiopeia.insert = {
+  a = { bg = colors.blue, fg = colors.black, gui = 'bold' },
+  b = { bg = colors.lightgray, fg = colors.white },
+  c = { bg = colors.darkgray, fg = colors.white },
+}
+cassiopeia.visual = {
+  a = { bg = colors.yellow, fg = colors.black, gui = 'bold' },
+  b = { bg = colors.lightgray, fg = colors.white },
+  c = { bg = colors.darkgray, fg = colors.white },
+}
+cassiopeia.replace = {
+  a = { bg = colors.red, fg = colors.black, gui = 'bold' },
+  b = { bg = colors.lightgray, fg = colors.white },
+  c = { bg = colors.darkgray, fg = colors.white },
+}
+cassiopeia.command = {
+  a = { bg = colors.green, fg = colors.black, gui = 'bold' },
+  b = { bg = colors.lightgray, fg = colors.white },
+  c = { bg = colors.darkgray, fg = colors.white },
+}
+cassiopeia.inactive = {
+  a = { bg = colors.inactivegray, fg = colors.gray, gui = 'bold' },
+  b = { bg = colors.inactivegray, fg = colors.gray },
+  c = { bg = colors.inactivegray, fg = colors.gray },
+}
+
+return cassiopeia
